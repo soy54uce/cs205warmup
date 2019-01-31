659194ee3b8ea2d56fef1605c80fbec07bdda9fb
soy54uce <beaudfin@gmail.com>
2019-01-31 16:38:24 +0000

    add more input options

--- main.py
+++ main.py
@@ -20,12 +20,28 @@ def parse_input(my_input_param):
             display_help()
         else:
             print("not a valid command, type help for more information")
-    if len(input_array) == 2:
+    elif len(input_array) == 2:
         # simple 2 arg queries
         if input_array[0] == "county":
             simple_county_query(input_array)
         elif input_array[0] == "seat":
             simple_county_seat_query(input_array)
+        else:
+            print("not a valid command, type help for more information")
+    elif len(input_array) == 3:
+        if input_array[0] == "county":
+            county_attribute_query(input_array)
+        elif input_array[0] == "seat":
+            county_seat_attribute_query(input_array)
+        else:
+            print("not a valid command, type help for more information")
+    elif len(input_array) == 4:
+        if (input_array[0] == "county") & (input_array[2] == "seat"):
+            county_to_seat_join(input_array)
+        elif (input_array[0] == "seat") & (input_array[2] == "county"):
+            seat_to_county_join(input_array)
+        else:
+            print("not a valid command, type help for more information")
     return
 
 
@@ -36,15 +52,17 @@ def display_help():
         "help": "display all commands and what they do",
         "schema": "list the database tables and column names",
         "counties": "display entire table of california counties",
-        "seats": "display entire table of california county seats",
         "county COUNTY": "search county table for specified county COUNTY, return entire row",
         "county ATTR COUNTY": "search county table for specified county COUNTY, return ATTR column value",
+        "county COUNTY seat SEAT": "search county table for COUNTY, join on seat SEAT",
+        "seats": "display entire table of california county seats",
         "seat SEAT": "search county seat table for specified county seat SEAT, return entire row",
         "seat ATTR SEAT": "search county seat table for specified county seat SEAT, return ATTR column value",
+        "seat SEAT county COUNTY": "search seat table for SEAT, join on county COUNTY"
         # will add more help info after i get a schema of the database
     }
     for command in command_dict:
-        print("{:>18s}   {}".format(command, command_dict[command]))
+        print("{:>23s}   {}".format(command, command_dict[command]))
     return
 
 
@@ -74,6 +92,25 @@ def simple_county_query(some_input_array):
 def simple_county_seat_query(some_input_array):
     print("checking county seat table for", some_input_array[1])
     # build and execute query of county seat table
+    return
+
+
+def county_attribute_query(some_input_array):
+    print("checking county table for column", some_input_array[1], "where county is", some_input_array[2])
+    return
+
+
+def county_seat_attribute_query(some_input_array):
+    print("checking county seat table for column", some_input_array[1], "where county seat is", some_input_array[2])
+
+
+def county_to_seat_join(some_input_array):
+    print("checking county table for", some_input_array[1], "where county seat is", some_input_array[3])
+    return
+
+
+def seat_to_county_join(some_input_array):
+    print("checking county seat table for", some_input_array[1], "where county is", some_input_array[3])
     return
 
 
