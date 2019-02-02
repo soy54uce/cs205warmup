1e5617f4c15b54ae1d864fb66ad5dd0eb875ccc9
Barry F Smith <21973169+bfsmith9@users.noreply.github.com>
2019-02-01 23:32:44 -0500

    Database, python-SQL function
    * Database built and working
    * Starting to get Python and sqlite talking

--- sqlProcess.py
+++ sqlProcess.py
@@ -1,10 +1,28 @@
+#! /usr/bin/env python3
+
 import sqlite3
 
-conn = sqlite3.connect('counties.db')
+# Test whether SQL and our dbase will work
+def do_query_test():
+    
+    # Create a connection
+    conn = sqlite3.connect('cali.db')
+    
+    # Create a cursor
+    c = conn.cursor()
 
-def do_Query():
-    print("Hello, here is a query.")
+    c.execute('SELECT * FROM COUNTIES')
+    for row in c.execute('SELECT * FROM COUNTIES'):
+        print(row)
+    
+def do_query(some_input_array):
+    
+    # Create a connection
+    conn = sqlite3.connect('cali.db')
+    
+    # Create a cursor
+    c = conn.cursor()
+
     
     
-    
-do_Query()
+do_query_test()
