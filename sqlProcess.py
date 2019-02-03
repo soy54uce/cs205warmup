3a24a056cd2c70b3061ed711e3c28b7482b0df18
Barry F Smith <21973169+bfsmith9@users.noreply.github.com>
2019-02-03 16:23:42 -0500

    IF-STATEMENTS STARTED BASED ON KIND OF REQUEST Right now, depending on the length of the input array, we'll vary the kinds of SQL statements run.

--- sqlProcess.py
+++ sqlProcess.py
@@ -1,10 +1,10 @@
-#########################################################
-# WARMUP - SQL<->PYTHON - CS205                         #
-# CODE TO LINK SQLITE QUERIES & PYTHON                  #
-# BARRY SMITH - FEBRUARY 2019                           #
-#########################################################
+#! /usr/local/bin/python3
 
-#! /usr/local/bin python3
+#########################################################
+# WARMUP    -    SQL<->PYTHON    -    CS205             #
+# CODE TO LINK SQLITE QUERIES & PYTHON                  #
+# BARRY SMITH - CS205 - FEBRUARY 2019                   #
+#########################################################
 
 import sqlite3
 
@@ -26,28 +26,32 @@ def do_query(table, some_input_array):
     conn = sqlite3.connect('cali.db')
     c = conn.cursor()
 
-    # "Clearing" code to get rid of brackets/quotes around array vars
-    # https://stackoverflow.com/questions/29642188/removing-the-square-brackets-commas-and-single-quote
-    attrib = (some_input_array[0])
-    attrib_cleared = (" ".join(some_input_array[0]))
-    print(attrib_cleared)
-    value_cleared = (" ".join(some_input_array[1]))
-    print(value_cleared)
+    # If we're looking for a simple attribute of a single table,
+    # there will only be two values in the array
 
-    # Working simple query
-    # c.execute("select population from counties where \"County seat\" = 'Sonora'")
+    if len(some_input_array) == 2:
+        # "Clearing" code to get rid of brackets/quotes around array vars
+        # https://stackoverflow.com/questions/29642188/removing-the-square-brackets-commas-nd-single-quote
+        attrib = (some_input_array[0])
+        attrib_cleared = (" ".join(some_input_array[0]))
+        print(attrib_cleared)
+        value_cleared = (" ".join(some_input_array[1]))
+        print(value_cleared)
 
-    # Working simple query with variable sub
-    c.execute("select {} from {} where \"County seat\" = {}".format(attrib_cleared, table, value_cleared))
+        # Working simple query
+        # c.execute("select population from counties where \"County seat\" = 'Sonora'")
 
-    print(c.fetchone())
+        # Working simple query with variable sub
+        c.execute("select {} from {} where \"Name\" = {}".format(attrib_cleared, table, value_cleared))
 
-    # Working join
-    c.execute("select seats.population from counties, seats where counties.name = seats.county and counties.name = 'Yuba County'")
-    print(c.fetchone())
+        print(c.fetchone())
+
+        # Working join
+        c.execute("select seats.population from counties, seats where counties.name = seats.county and counties.name = 'Yuba County'")
+        print(c.fetchone())
 
 
-# INPUT TESTING BELOW HERE ---------------------
+# INPUT TESTING BELOW HERE -----------------------------------------
 
 # What table are we going to use? Will depend on input string. Pick this up from Ben's code - here, we'll say it's "county"
 table_choice = "county"
@@ -56,6 +60,6 @@ if table_choice == "county":
 else:
     table = "seats"
 
-# Pretend user wants pop of Sonora County
-some_input_array = ["population"],["'Sonora'"]
+# Pretend user wants pop of Los Angeles County
+some_input_array = ["population"],["'Los Angeles County'"]
 do_query(table, some_input_array)
