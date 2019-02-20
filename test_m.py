9949e1f66f8afbccc275197ea52783301fe74b27
Ben Michael Beaudin <bbeaudin@uvm.edu>
2019-02-20 18:57:32 -0500

    adjust test cases to match new ouput

--- test_m.py
+++ test_m.py
@@ -26,47 +26,50 @@ class Test(TestCase):
     def test_counties_all(self):
         query = "counties"
         result = test_query(query).split('\n')
-        self.assertEqual(len(result), 58)
+        self.assertEqual(len(result), 61)
 
     def test_seats_all(self):
         query = "seats"
         result = test_query(query).split('\n')
-        self.assertEqual(len(result), 58)
+        self.assertEqual(len(result), 61)
 
     def test_counties_query(self):
         query = "county Solano County"
         result = test_query(query)
-        self.assertEqual(result, "('48', 'Solano County', 'Fairfield', '445,458', '1850')")
+        resultArr = result.split('\n')
+        self.assertEqual(resultArr[2], "48   Solano County            Fairfield                445,458             1850")
+
 
     def test_seats_query(self):
         query = "seat Woodland"
         result = test_query(query)
-        self.assertEqual(result, "('57', 'Woodland', '55,468', 'Yolo County')")
+        resultArr = result.split('\n')
+        self.assertEqual(resultArr[2], "57   Woodland                 55,468                   Yolo County")
 
     def test_county_attribute(self):
         query = "county id Solano County"
         result = test_query(query)
-        self.assertEqual(result, "('48',)")
+        self.assertEqual(result, "The id of Solano County is: 48")
 
     def test_seats_attribute(self):
         query = "seat population Crescent City"
         result = test_query(query)
-        self.assertEqual(result, "('7,643',)")
+        self.assertEqual(result, "The population of Crescent City is: 7,643")
 
     def test_county_join_seat(self):
         query = "county established seat Downieville"
         result = test_query(query)
-        self.assertEqual(result, "('1852',)")
+        self.assertEqual(result, "The county with seat Downieville was established in: 1852")
 
     def test_seat_join_county(self):
         query = "seat population county Alameda County"
         result = test_query(query)
-        self.assertEqual(result, "('390,724',)")
+        self.assertEqual(result, "The population of the Alameda County seat is: 390,724")
 
     def test_not_found(self):
         query = "seat population county UnKnown County"
         result = test_query(query)
-        self.assertEqual(result, "None")
+        self.assertEqual(result, "Invalid county or seat")
 
     def test_error(self):
         query = "county Id yuba County"
@@ -76,7 +79,7 @@ class Test(TestCase):
     def test_exception(self):
         query = "seat iid Crescent City"
         result = test_query(query)
-        self.assertEqual(result, "no such column: iid")
+        self.assertEqual(result, "Invalid attribute")
 
 if __name__ == "__main__":
     unittest.main()
